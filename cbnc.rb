3b288553966c91d523e1b3556afe6449a09b45ed
Steven <ekczile@gmail.com>
2018-10-08 16:07:39 -0400

    First Commit

--- cbnc.rb
+++ cbnc.rb
@@ -1,8 +1,9 @@
-def grand_bash(my_number, bash_nuber)
+def grand_bash(my_numbers, bash_numbers)
  array1 = []
     if 
-    bash_numbers.include? (my_numbers)
-    array<< my_numbers
+    bash_numbers.include?(my_numbers)
+    array1<< my_numbers
     end
     p array1
 end
+
--- cbnctest.rb
+++ cbnctest.rb
@@ -2,14 +2,57 @@ require_relative "cbnc.rb"
 require "minitest/autorun"
 
 class Test_cbnc < Minitest::Test
-
-    def 
+    def test_for_1_equals_1
         assert_equal(1,1)
     end
-
-    def 
+    def test_for_empty_array
         my_numbers = "1234"
         bash_numbers = []
-        assert_equal("", grand_bash(my_numbers, bash_numbers))
+        assert_equal([], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_1234
+        my_numbers = "1234"
+        bash_numbers = ["1234"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_3_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_3_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_4_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455", "4123"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_5_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455", "4123", "1234"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_6_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455", "4123", "4123", "6534"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_7_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455", "4123", "4123", "6534", "1234"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_8_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455", "4123", "4123", "6534", "5466", "9999"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
+    end
+    def test_array_of_9_numbers
+        my_numbers = "1234"
+        bash_numbers = ["1233", "1345", "3455", "4123", "4123", "6534", "5466", "9999", "1234"]
+        assert_equal(["1234"], grand_bash(my_numbers, bash_numbers))
     end
 end
